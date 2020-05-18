b9fc3594b3c276e905d3596c467ecd6a06e652a3
romagny13 <romagny13@yahoo.fr>
2020-05-18 21:47:14 +0200

    ShellService updated

--- PrismDelayedScopedRegionCreationBehavior/Prism/Regions/ShellService.cs
+++ PrismDelayedScopedRegionCreationBehavior/Prism/Regions/ShellService.cs
@@ -1,14 +1,24 @@
 ﻿using CommonServiceLocator;
 using Prism.Common;
+using System;
 using System.Windows;
 
 namespace Prism.Regions
 {
     public class ShellService : IShellService
     {
-        public void ShowShell<T>() where T : Window
+        private IServiceLocator locator;
+
+        public ShellService(IServiceLocator locator)
         {
-            IServiceLocator locator = ServiceLocator.Current;
+            if (locator is null)
+                throw new ArgumentNullException(nameof(locator));
+
+            this.locator = locator;
+        }
+
+        public virtual void ShowShell<T>() where T : Window
+        {
             IRegionManager regionManager = new RegionManager();
 
             var window = locator.GetInstance<T>();
@@ -18,7 +28,14 @@ namespace Prism.Regions
             // try to Set IRegionManagerAware for Shell ViewModel
             MvvmHelpers.ViewAndViewModelAction<IRegionManagerAware>(window, v => v.RegionManager = regionManager);
 
+            InitializeShell(window);
+
             window.Show();
+        }
+
+        protected virtual void InitializeShell(Window shell)
+        {
+
         }
     }
 }

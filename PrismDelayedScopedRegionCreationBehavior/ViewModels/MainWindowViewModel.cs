c336dd216ab93f4a0ac19ca6f66e817f6b8d85aa
romagny13 <romagny13@yahoo.fr>
2020-05-18 13:18:32 +0200

    New Shell Sample

--- PrismDelayedScopedRegionCreationBehavior/ViewModels/MainWindowViewModel.cs
+++ PrismDelayedScopedRegionCreationBehavior/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,43 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using PrismDelayedScopedRegionCreationBehavior.Views;
 
 namespace PrismDelayedScopedRegionCreationBehavior.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
         private string _title = "Prism Application";
+        private DelegateCommand showShellCommand;
+
+        public MainWindowViewModel()
+        {
+
+        }
+
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
-        public MainWindowViewModel()
-        {
 
+        public DelegateCommand ShowShellCommand
+        {
+            get
+            {
+                if (showShellCommand == null)
+                    showShellCommand = new DelegateCommand(ExecuteShowShellCommand);
+                return showShellCommand;
+            }
         }
+
+        private void ExecuteShowShellCommand()
+        {
+            var window = new MainWindow();
+            RegionManager.SetRegionManager(window, new RegionManager());
+            window.Show();
+        }
+
     }
 }
